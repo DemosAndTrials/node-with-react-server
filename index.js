e4608e27a84bed32395626a6052f613b0f453985
Ros Haitovich <matrostik@gmail.com>
2019-02-02 21:51:40 +0200

    google auth + mongoose

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mangoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
+mangoose.connect(keys.mongoUri);
+
 const app = express();
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+authRoutes(app);
 
-const PORT = process.env.PORT || 5000
+const PORT = keys.port;
 app.listen(PORT);
