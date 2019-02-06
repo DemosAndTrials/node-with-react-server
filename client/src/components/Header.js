93db124796d9987dcbad90a9a69b4b8f7f02600b
Ros Haitovich <matrostik@gmail.com>
2019-02-06 23:02:02 +0200

    lesson 80

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component{
             <nav>
             <div className="nav-wrapper">
               <a href="/" className="brand-logo">Logo</a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a>Login With Google</a></li>
               </ul>
             </div>
